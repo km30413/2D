ee12294108c03aff6f70f7ae197eb90851112ae6
matsuda <matsuda@matsudanomacbook-puro.local>
2018-04-21 07:45:56 -0700

    Initial Commit

---
+++ create2Dgraphics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  create2Dgraphics
+//
+//  Created by matsuda on 2018/04/21.
+//  Copyright © 2018年 matsuda. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ create2Dgraphics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  create2Dgraphics
+//
+//  Created by matsuda on 2018/04/21.
+//  Copyright © 2018年 matsuda. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
